e7172dd4c7b8f0fb014b7c50c96e0a5da5fc5621
Andy Tu <andytu@alumni.ubc.ca>
2015-08-11 17:55:45 -0700

    add card to display photo on click(wip). create mini gallery of photos to display

---
+++ index.js
@@ -0,0 +1,11 @@
+(function () {
+
+    var challengeGallery = document.getElementById('challenge-gallery');
+
+    console.log(challengeGallery);
+
+    challengeGallery.addEventListener('click', function () {
+        console.log(event);
+    }, false);
+
+})();
